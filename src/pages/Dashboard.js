11af272545f9f2dade1a9ec507177f29442c83f1
veyseltosun <postalarim1923@gmail.com>
2022-12-26 22:53:03 +0300

    app router before the auth.

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import BlogCard from '../components/BlogCard'
+
+const Dashboard = () => {
+  return (
+    <div>
+        <BlogCard/>
+    </div>
+  )
+}
+
+export default Dashboard
